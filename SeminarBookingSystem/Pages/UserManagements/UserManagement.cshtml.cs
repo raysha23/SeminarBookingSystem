0078082e030633eb78ee49b88fc4281c6512b238
raysha23 <sismundoraymund@gmail.com>
2026-03-18 00:23:05 +0800

    UI dashboard & User Management, done

---
+++ SeminarBookingSystem/Pages/UserManagements/UserManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeminarBookingSystem.Pages.UserManagement
+{
+    public class UserManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
